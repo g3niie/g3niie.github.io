35f344c761bb4256e84aba000f610e75b83adc01
FrootyJyotish <116287202+g3niie@users.noreply.github.com>
2023-06-16 05:19:03 +0200

    Hele about me page afgemaakt
    met deze about me page, heb ik het hele about me page afgemaakt.

--- JS/aboutme.js
+++ JS/aboutme.js
@@ -3,12 +3,30 @@ console.log('File loaded');
 const lightSwitch = document.querySelector('div.light-switch');
 const wrapper = document.querySelector('div.wrapper');
 const h1 = document.querySelector('h1');
+const p = document.querySelectorAll('p');
+const switchContainer = document.querySelector('switch-container');
+const switchCaseContainer1 = document.querySelector('div.switch-case-1');
+const switchCaseContainer2 = document.querySelector('div.switch-case-2');
+const button1 = document.querySelector('button.switch-case-button1');
+const button2 = document.querySelector('button.switch-case-button2');
+const homepage = document.querySelector('button.homepage');
 
-lightSwitch.addEventListener('click', addMargin);
-function addMargin(){
+lightSwitch.addEventListener('click', mainPage);
+button1.addEventListener('click', nextPage);
+homepage.addEventListener('click', goHome);
+function mainPage(){
     lightSwitch.classList.toggle('margin');
+    button1.classList.remove('hidden');
     wrapper.style.backgroundColor = 'white';
     lightSwitch.style.backgroundColor = 'black';
     document.body.style.backgroundColor = 'black';
     h1.style.color = 'white';
+    homepage.style.backgroundColor = 'white';
+    homepage.style.color = 'black';
+}
+function nextPage(){
+    switchCaseContainer2.classList.toggle('hidden');
+}
+function goHome(){
+    location.href = 'index.html'
 }
