f9b6e0e11f9258a955a5caf63ca558b457144b60
FrootyJyotish <116287202+g3niie@users.noreply.github.com>
2023-06-17 15:43:35 +0200

    HELE WEBSITE KLAAR
    met deze commit en push heb ik het hele website afgemaakt met alles ingezet

--- JS/contact.js
+++ JS/contact.js
@@ -1,15 +1,18 @@
 console.log('File loaded');
 
+//alle elementen van HTML bestand opgenomen
 const discord = document.querySelector('div.discord');
 const github = document.querySelector('div.github');
 const email = document.querySelector('div.email');
 const linkedIn = document.querySelector('div.linked-in');
 
+//alle addEventListeners aangezet om naar het website te gaan
 discord.addEventListener('click', goToDiscord);
 github.addEventListener("click", goToGithub);
 email.addEventListener("click", goToEmail);
 linkedIn.addEventListener('click', goToLinkedIn);
 
+//alle functions van addEventListeners aangezet zodan het jou naar het website brengt
 function goToDiscord(){
     window.location.href = 'https://discord.gg/egirl';
 }
